ac413fb911489c1fd3e7a6bb2c202653daa8cbe7
Joshua Huston <joshua.david.huston@gmail.com>
2022-02-10 01:01:04 -0500

    Completed all Lab 18 reqts

---
+++ README.md
@@ -0,0 +1,16 @@
+# LAB - 18
+
+## Caesar Cipher
+
+Functions:
+
+- encrypt: encrypts a phrase with a Caesarian cypher using a given key
+- decrypt: decrypts phrase with given key
+- crack: brute force decryption without using key
+- work_check: uses nltk to check words in the crack function to parse through nonsense phrases
+
+### Author: Joshua Huston
+
+### Reflections and Comments
+
+This was a good lab, lots of challenging features but overall manageable with some pointers in the right direction. I decided to come up with a separate function to assist the crack function in checking words against nltk. My solution used ASCII values for encryption.
--- caesar_cipher/corpus.py
+++ caesar_cipher/corpus.py
@@ -9,24 +9,31 @@ from nltk.corpus import words, names
 nltk.download('words', quiet=True)
 nltk.download('names', quiet=True)
 
+def word_check(phrase):
 
-words_list = words.words()
-names_list = names.words()
-# print(words_list)
+  words_list = words.words()
+  names_list = names.words()
+  # print(words_list)
 
-test_sentence = 'It was the best of times, it was the worst of times.'
-word_list_test = test_sentence.split(' ')
+  
+  word_list_test = phrase.split(' ')
 
-hits = 0
-num_words = 0
+  hits = 0
+  num_words = 0
 
-for word in word_list_test:
-    if word.lower() in words_list:
-        print(f'The word {word} is in the list')
-        hits += 1
-        num_words += 1
-    else:
-        print(f'The word {word} is not in the list')
-        num_words += 1
+  for word in word_list_test:
+      if word.lower() in words_list:
+          # print(f'The word {word} is in the list')
+          hits += 1
+          num_words += 1
+      else:
+          # print(f'The word {word} is not in the list')
+          num_words += 1
 
-print(f"{hits/num_words * 100}%")
+  # print(f"{hits/num_words * 100}%")
+  return (hits/num_words * 100)
+
+
+
+if __name__ == "__main__":
+  print(word_check('It was the best of times, it was the worst of times.'))
