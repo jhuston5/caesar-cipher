0bc0a9e7c63642c7535f7ca8dcc9e450fe84a410
Joshua Huston <joshua.david.huston@gmail.com>
2022-02-09 23:03:48 -0500

    Encrypt and Decrypt functions working

--- caesar_cipher/cipher.py
+++ caesar_cipher/cipher.py
@@ -1,6 +1,4 @@
-import nltk
 
-nltk.download()
 
 def encrypt(plain, key):
   # Shift the plain text
@@ -11,12 +9,26 @@ def encrypt(plain, key):
 
   for char in plain:
     # print(f'plain char of {char} not yet shifted by {key}')
-    encrypted_char = int(char)
-    encrypted_char += key
-    if encrypted_char > 9:
-      encrypted_char = (char + key) % 10
-    encrypted_text += str(encrypted_char)
-    # print(f'plain char of {encrypted_char} shifted by {key}')
+    if char.isupper():
+      char_idx = ord(char) - ord('A')
+
+      shift = (char_idx + key) % 26 + ord('A')
+
+      new_char = chr(shift)
+      encrypted_text += new_char
+   
+    elif char.islower():
+      char_idx = ord(char) - ord('a')
+
+      shift = (char_idx + key) % 26 + ord('a')
+
+      new_char = chr(shift)
+      encrypted_text += new_char
+    
+    else:
+      encrypted_text += char
+
+    
   print(encrypted_text)
   return encrypted_text
 
@@ -25,9 +37,15 @@ def decrypt(encrypted, key):
   return encrypt(encrypted, -key)
 
 def crack(encrypted):
+  # set the key to be 
   pass
 
 if __name__ == "__main__":
-  enc1 = encrypt('12345', 2)
-  assert enc1 == ('34567')
+  # enc1 = encrypt('APPLE', 1)
+  # assert enc1 == ('34567')
 
+  # enc2 = encrypt('aP%P5le', 1)
+  # assert enc2 == ('34567')
+
+  dec1 = decrypt('bQ%  Q5mf', 1)
+  assert dec1 == 'Apple'
--- caesar_cipher/corpus.py
+++ caesar_cipher/corpus.py
@@ -5,6 +5,28 @@ from nltk.corpus import words, names
 # nltk.download()
 
 
-words_list = words.words()
 
-print(words_list)
+nltk.download('words', quiet=True)
+nltk.download('names', quiet=True)
+
+
+words_list = words.words()
+names_list = names.words()
+# print(words_list)
+
+test_sentence = 'It was the best of times, it was the worst of times.'
+word_list_test = test_sentence.split(' ')
+
+hits = 0
+num_words = 0
+
+for word in word_list_test:
+    if word.lower() in words_list:
+        print(f'The word {word} is in the list')
+        hits += 1
+        num_words += 1
+    else:
+        print(f'The word {word} is not in the list')
+        num_words += 1
+
+print(f"{hits/num_words * 100}%")
